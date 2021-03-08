8d838d1bb9c79f6b6464f1f64bba21213f194957
asfuture <asfuture2019@gmail.com>
2021-03-08 13:41:42 -0300

    aula-angular

--- aula-angular/src/app/app.module.ts
+++ aula-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CursoListCompoent } from './cursos/curso-list.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoListCompoent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
